dddfc1f1cc99b2c34f0dc6ef8675cd1d998a000a
Cecilia <cecilia4binck@gmail.com>
2020-06-11 21:39:53 +0200

    Added Timestamp, index page and created users

--- run.py
+++ run.py
@@ -1,22 +1,38 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
+
+if os.path.exists("env.py"):
+	import env
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") 
 messages = []
 
 """Add messages to the `messages` list"""
 def add_messages(username, message):
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+
 
 def get_all_messages():
     """Get all of the messages and separate them with a `br`"""
     return "<br>".join(messages)
 
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with instruction"""
-    return "To send a message use /USERNAME/MESSAGE"
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
+    
 
 
 @app.route("/<username>")
@@ -33,4 +49,4 @@ def send_message(username, message):
     return redirect("/" + username)
 
 
-app.run(host=os.getenv("IP"), port=os.getenv("PORT"), debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=os.getenv("PORT", "5000"), debug=True)
